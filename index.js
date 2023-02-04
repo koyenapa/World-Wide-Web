f14c4808d31f62b8f5d575049720e1b65505c7d3
koyenapa <121396381+koyenapa@users.noreply.github.com>
2023-02-04 12:56:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var str =""
+for(let i = 1;i<=4;i++){
+
+    for(k=4;k>i;k--){
+        str+=' '
+    }
+
+    for(j=1;j<=i;j++){
+        str+='* '
+    }
+    str+="\n"
+
+}
+console.log(str)
+
+ var str1=""
+ for(let i = 1;i<=4;i++){
+    for(j=1;j<=4;j++){
+        str1+='*'
+    }
+    str1+="\n"
+ }
+ console.log(str1)
+
+
